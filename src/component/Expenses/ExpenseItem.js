dcdc2a62866fed2b73f2f7f33d9489a6c2abc3db
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-10-27 21:55:12 +0300

    add on click event

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -13,6 +13,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={ () => alert("HI!") }>Change Title</button>
         </Card>
     )
 }
