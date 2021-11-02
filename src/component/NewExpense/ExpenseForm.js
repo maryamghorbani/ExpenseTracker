ea950d96796524e34939697a87ec548486d2f28d
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-02 10:55:58 +0200

    add Cancle button in add expense form

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -49,6 +49,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button>Cancle</button>
                 <button type='submit '>Add Expense</button>
             </div>
         </form>
