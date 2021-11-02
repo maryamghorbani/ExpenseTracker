3bf623ec5d2ffc6f2b9772bfe28a3aa944f622ea
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-02 15:50:47 +0200

    complete chart bar component

--- src/component/Chart/Chart.js
+++ src/component/Chart/Chart.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import ChartBar from "./ChartBar";
+
 import './Chart.css';
 
-const Chart = () => {
-
-}
+const Chart = props => {
+    return (
+        <div className="chart">
+            {props.dataPoints.map(dataPoints => (
+                <ChartBar
+                    key={dataPoints.id}
+                    vlaue={dataPoints.value}
+                    maxValue={null}
+                    lable={dataPoints.label}
+                />
+            ))}
+        </div>
+    )
+};
 export default Chart;
--- src/component/Chart/ChartBar.js
+++ src/component/Chart/ChartBar.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 import './ChartBar.css';
 
-const ChartBar = () => {
-
-}
+const ChartBar = props => {
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill"></div>
+                <div className="chart-bar__label"></div>
+            </div>
+        </div>
+    )
+};
 export default ChartBar;
