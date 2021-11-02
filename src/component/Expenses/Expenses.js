2212ff0ed0a043b6fdb285cabc5773c036851e51
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-02 08:57:19 +0200

    refactor condition

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -17,22 +17,32 @@ const Expenses = (props) => {
     const filteredExpense = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-    
+    let expensesContent = <p>No expenses found.</p>;
+    if (filteredExpense.length > 0) {
+        expensesContent = filteredExpense.map(expense => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}/>
+        ));
+    }
 
     return (
 
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {filteredExpense.length === 0 && <p>No expenses found.</p>}
-            {filteredExpense.length > 0 &&
-                filteredExpense.map(expense => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}/>
-                )
-            )}
+            {/*{filteredExpense.length === 0 && <p>No expenses found.</p>}*/}
+            {/*{filteredExpense.length > 0 &&*/}
+            {/*filteredExpense.map(expense => (*/}
+            {/*        <ExpenseItem*/}
+            {/*            key={expense.id}*/}
+            {/*            title={expense.title}*/}
+            {/*            amount={expense.amount}*/}
+            {/*            date={expense.date}/>*/}
+            {/*    )*/}
+            {/*)}*/}
+            {expensesContent}
         </Card>
     )
 
