1860a38201d7a5407d0c39cb6d5d13a8c01241c4
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-11-02 19:00:04 +0200

    create expenses chart component

--- src/component/Chart/ChartBar.js
+++ src/component/Chart/ChartBar.js
@@ -3,12 +3,18 @@ import React from 'react';
 import './ChartBar.css';
 
 const ChartBar = props => {
+    let barFillHeight = '0%';
+
+    if (props.max > 0) {
+        barFillHeight = Math.round((props.vlaue / props.maxValue) * 100) + '%';
+    }
+
     return (
         <div className="chart-bar">
             <div className="chart-bar__inner">
-                <div className="chart-bar__fill"></div>
-                <div className="chart-bar__label"></div>
+                <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
             </div>
+            <div className="chart-bar__label">{props.lable}</div>
         </div>
     )
 };
---
+++ src/component/Expenses/ExpensesChart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Chart from "../Chart/Chart";
+
+const ExpensesCart = props => {
+    return <Chart/>
+};
+
+export default ExpensesCart;
